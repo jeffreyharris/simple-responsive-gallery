7c5e1c4248fdcb452aeea2b7044b945c7cec0a2d
Jeff <jeffrey.david.harris@gmail.com>
2015-01-26 15:58:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-  $(function(){
-	$('.galPhoto').hide();
-	$('.galPhoto:first').show();
-        $('.singleItem').click(function(){  
-            $('.galPhoto').hide();  
-			clickedStory = $('#'+ $(this).attr('target'))
-			clickedStory.show();
-			$('html,body').animate({scrollTop: clickedStory.offset().top-10},'slow');
-        });
-});
+var JEFF = JEFF || {};
 
+$('.galPhoto').hide();
+$('.galPhoto:first').show();
+	
+JEFF.simpleGallery = function($el){  
+	$photo = $('#'+ $el.attr('target'));
+		
+	$('.galPhoto').hide( 0, function() {
+		$photo.show();
+	});
+		
+	$('html,body').animate({scrollTop: $photo.offset().top-10},'slow');
+}
+
+$(document).ready(function() {
+	$('.singleItem').bind('click', function(e) { 
+		e.preventDefault();
+		JEFF.simpleGallery($(this));
+	});
+});
